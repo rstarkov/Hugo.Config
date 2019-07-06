63b74db5d4578317db0612f84314d6fd77b5b4e6
Roman Starkov <sourcecode@roman.st>
2019-07-06 08:51:04 +0100

    Basic module config implemented

---
+++ src/Config.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using RT.Util.Serialization;
+
+namespace Hugo.Config
+{
+    public interface IModuleConfig
+    {
+    }
+
+    static class ModuleConfigProvider
+    {
+        private static object LoadSettings(IServiceProvider provider, Type configType)
+        {
+            var filename = $"config.{configType.Name.Replace("Config", "")}.json";
+            if (File.Exists(filename))
+                return ClassifyJson.DeserializeFile(configType, filename);
+            else
+            {
+                var result = configType.GetConstructor(Type.EmptyTypes).Invoke(new object[0]);
+                ClassifyJson.SerializeToFile(configType, result, filename);
+                return result;
+            }
+        }
+
+        public static void AddAll(IServiceCollection services)
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes().Where(t => typeof(IModuleConfig).IsAssignableFrom(t) && !t.IsAbstract);
+            foreach (var type in types)
+                services.Add(new ServiceDescriptor(type, provider => LoadSettings(provider, type), ServiceLifetime.Singleton));
+        }
+    }
+}
